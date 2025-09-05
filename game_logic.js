704e4b13a79d54381bf3adfde46c215ed3b7465e
joaquinauc <joaquins_urrea@hotmail.com>
2025-09-05 08:58:43 -0600

    Add function to play a round of the game

--- game_logic.js
+++ game_logic.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice()
 {
     return Math.ceil(Math.random() * 3);
@@ -24,4 +27,32 @@ function getHumanChoice()
     return humanChoice;
 }
 
-console.log(getHumanChoice());
+function playRound(humanChoice, computerChoice)
+{
+    gameOptions = ["rock", "paper", "scissors"];
+
+    let humanChoiceString = gameOptions[humanChoice - 1];
+    let computerChoiceString = gameOptions[computerChoice - 1];
+
+    console.log(`The computer chose: ${computerChoiceString}`)
+
+    if (humanChoice > computerChoice)
+    {
+        humanScore++;
+        console.log(`You win! ${humanChoiceString} beats ${computerChoiceString}`);
+    }
+    else if (humanChoice < computerChoice)
+    {
+        computerScore++;
+        console.log(`You lose! ${computerChoiceString} beats ${humanChoiceString}`);
+    }
+    else
+    {
+        console.log(`It's a tie! ${humanChoiceString} doesn't beat ${computerChoiceString}`);
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
