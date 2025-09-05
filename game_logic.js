a9fa17893767b1d83d94cd83d5d4f71d491db60c
joaquinauc <joaquins_urrea@hotmail.com>
2025-09-05 08:33:56 -0600

    Add function to get human's choice

--- game_logic.js
+++ game_logic.js
@@ -3,3 +3,25 @@ function getComputerChoice()
     return Math.ceil(Math.random() * 3);
 }
 
+function getHumanChoice()
+{
+    let userInput = prompt("Choose Rock, Paper or Scissors!").toLowerCase();
+    let humanChoice;
+
+    switch (userInput)
+    {
+        case "rock":
+            humanChoice = 1;
+            break;
+        case "paper":
+            humanChoice = 2;
+            break;
+        case "scissors":
+            humanChoice = 3;
+            break;
+    }
+
+    return humanChoice;
+}
+
+console.log(getHumanChoice());
