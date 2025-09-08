ca8af64232e14d836806707956d53f22c25b7c25
joaquinauc <joaquins_urrea@hotmail.com>
2025-09-08 17:34:05 -0600

    Delete functions that are no longer of use

--- game_logic.js
+++ game_logic.js
@@ -6,27 +6,6 @@ function getComputerChoice()
     return Math.ceil(Math.random() * 3);
 }
 
-function getHumanChoice()
-{
-    let userInput = prompt("Choose Rock, Paper or Scissors!").toLowerCase();
-    let humanChoice;
-
-    switch (userInput)
-    {
-        case "rock":
-            humanChoice = 1;
-            break;
-        case "paper":
-            humanChoice = 2;
-            break;
-        case "scissors":
-            humanChoice = 3;
-            break;
-    }
-
-    return humanChoice;
-}
-
 function playRound(humanChoice, computerChoice)
 {
     gameOptions = ["rock", "paper", "scissors"];
@@ -51,33 +30,3 @@ function playRound(humanChoice, computerChoice)
         console.log(`It's a tie! ${humanChoiceString} doesn't beat ${computerChoiceString}`);
     }
 }
-
-function playGame()
-{
-    for (i = 0; i < 5; i++)
-    {
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-
-        playRound(humanSelection, computerSelection);
-    }
-
-    console.log(`Computer's score: ${computerScore}`);
-    console.log(`Your score: ${humanScore}`);
-
-    if (humanScore > computerScore)
-    {
-        console.log("Congratulations, you win!")
-    }
-    else if (humanScore < computerScore)
-    {
-        console.log("You lose! Please try again.")
-    }
-    else
-    {
-        console.log("It's a tie! Please try again.")
-    }
-}
-
-playGame();
-
